7bd2be25967e38d7e2488954647a619a14e4c2d7
Gustavo Sampaio <ghsampaio1105@gmail.com>
2021-11-08 19:13:47 -0300

    Token agora depois de 24 horas não funciona mais

--- config/routes.js
+++ config/routes.js
@@ -37,7 +37,7 @@ routes.post('/login', (req, res) => {
     const username = req.body.username
     const user = { name: username}
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 86400})
     res.json({ accessToken: accessToken})
 })
 
